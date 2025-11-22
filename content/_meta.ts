24924440bf9a08277838f22b4d682e8962f382bd
Sunny Hirai <thesunny@gmail.com>
2025-11-22 15:29:40 -0800

    Updated a bunch of pages

---
+++ content/_meta.ts
@@ -0,0 +1,12 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: "Welcome to Twux",
+  "getting-started": "Getting Started",
+  variants: "Variants",
+  typescript: "TypeScript",
+  "best-practices": "Best Practices",
+  "libraries": "Building Libraries",
+};
+
+export default meta;
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import nextra from "nextra";
 const withNextra = nextra({});
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   turbopack: {
     resolveAlias: {
       // Path to your `mdx-components` file with extension
